07c9a2e431b978b00c70d62752534bd70b81152e
Addison <shangxindi@gmail.com>
2024-07-21 10:16:25 +0800

    fix: hero height

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,19 +5,21 @@ const Home = () => {
   return (
     <div>
       {/* hero section */}
-      <div className="absolute top-0 left-0">
-        <div className="relative w-full h-full overflow-hidden">
-          <img
-            className="object-cover w-full h-full scale-125"
-            src={HERO_BACKGROUND}
-            alt="hero"
-          ></img>
-          {/* transparent filter */}
-          <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent via-60% to-black"></div>
+      <div className="min-h-[43.75rem] px-0 pt-40 pb-16">
+        <div className="absolute top-0 left-0">
+          <div className="relative w-full h-full overflow-hidden">
+            <img
+              className="object-cover w-full h-full scale-125"
+              src={HERO_BACKGROUND}
+              alt="hero"
+            ></img>
+            {/* transparent filter */}
+            <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent via-60% to-black"></div>
+          </div>
         </div>
       </div>
 
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center">
         <Login />
       </div>
     </div>

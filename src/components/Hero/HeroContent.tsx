5f5d8534661d3dc0f0308ccbb886ba2da5565346
Addison <shangxindi@gmail.com>
2024-08-24 14:44:47 +0800

    refactor: button

--- src/components/Hero/HeroContent.tsx
+++ src/components/Hero/HeroContent.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { PlayIcon } from '@heroicons/react/24/outline';
+import { InformationCircleIcon, PlayIcon } from '@heroicons/react/24/outline';
 
 interface HeroContentProps {
   title: string;
@@ -19,11 +19,14 @@ const HeroContent: React.FC<HeroContentProps> = ({
       <h1 className="mb-4 text-2xl font-bold md:text-5xl">{title}</h1>
       <p className="hidden w-1/2 mb-8 text-lg md:inline-block">{description}</p>
       <div className="flex space-x-4">
-        <button className="btn btn-primary" onClick={onCtaClick}>
-          <PlayIcon className="w-6 h-6" />
+        <button className="btn" onClick={onCtaClick}>
+          <PlayIcon className="w-5 h-5" />
           {t('hero.ctaLabel')}
         </button>
-        <button className="btn btn-neutral">{t('hero.moreInfo')}</button>
+        <button className="btn btn-neutral">
+          <InformationCircleIcon className="w-5 h-5" />
+          {t('hero.moreInfo')}
+        </button>
       </div>
     </div>
   );
